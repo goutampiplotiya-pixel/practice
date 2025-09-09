987ef383f22f2fdbf2612b30b31e122758ff44a1
Goutam Piplotiya <goutam.piplotiya@gammaedge.io>
2025-09-09 17:49:45 +0530

    latest changes

--- cypress/e2e/test2.cy.js
+++ cypress/e2e/test2.cy.js
@@ -3,7 +3,7 @@ describe('it should be login into the given site', ()  =>{
     it('should login with valid crendentials',() => {
 
         //cy.visit('http://business.adobe.com');
-        cy.visit('http://www..com');
+        cy.visit('http://www.adobeasdfasfd.com');
     
     })
 })
