243426b90eeef77e6ccb23668658aac765dac0bc
Goutam Piplotiya <goutam.piplotiya@gammaedge.io>
2025-09-09 15:08:39 +0530

    add this page on on test

---
+++ cypress/e2e/test2.cy.js
@@ -0,0 +1,9 @@
+describe('it should be login into the given site', ()  =>{
+
+    it('should login with valid crendentials',() => {
+
+        //cy.visit('http://business.adobe.com');
+        cy.visit('http://www..com');
+    
+    })
+})
